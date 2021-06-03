237509652dbf156c7d81422b409824ecea66bb96
cg33 <chg80333@163.com>
2021-06-03 11:54:19 +0800

    initial

---
+++ cli_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/GoAdminGroup/go-admin/modules/system"
+	"github.com/magiconair/properties/assert"
+)
+
+func TestGetLatestVersion(t *testing.T) {
+	assert.Equal(t, getLatestVersion(), system.Version())
+}
